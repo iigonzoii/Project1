304906e0d308a4837ff7f4ba7454d58e77b801f8
iigonzoii <a.gonzalez418@gmail.com>
2021-02-06 18:31:52 -0700

    added five day fetch as a function and called it in our clickevent

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,36 +1,32 @@
-// var apikey = "4d2069688cmsh2f088ecb201529bp1f9b94jsn1b274d26bcd1"
 
 document.querySelector(".search-btn").addEventListener("click", function (event) {
     console.log("works")
     fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/SFO-sky/LAX-sky/2021-03-01?inboundpartialdate=2021-03-01", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "803484f99cmsh3c80d518e43f9cbp106fa0jsnc4dcb9639523",
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "803484f99cmsh3c80d518e43f9cbp106fa0jsnc4dcb9639523",
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        }
+    })
+        .then(response => {
+            console.log(response.json());
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    fiveDay()
 });
-    // event.preventDefault()
-    // // var selectedCity = document.querySelector("#search-term").value
-    // // console.log(selectedCity);
-    // // weatherSearch(selectedCity)
 
-    // // event listener goes here
-    // // var apiUrl = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/SFO-sky/LAX-sky/2021-03-01?=&=`
-    // fetch(apiUrl)
-    //     .then(function (response) {
-    //         if (response.ok) {
-    //             return response.json()
-    //         } else {
-    //             alert(`error: ${response.statusTest}`)
-    //         }
-    //     })
-    //     .then(function (data) {
-    //         console.log(data)
-    //     })
-});
+function fiveDay() {
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=denver&appid=1ab5d5fb108148020574fbedf5d8f664&units=imperial", requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+}
